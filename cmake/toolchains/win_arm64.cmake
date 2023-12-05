16206dd32309e246db62d6d8492cc429c9ad62e8
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2023-12-05 22:41:35 +0100

    prevent overwrite without system-name

--- cmake/toolchains/win_arm64.cmake
+++ cmake/toolchains/win_arm64.cmake
@@ -2,3 +2,4 @@
 # parameters to allow normal and cross-compilation to ARM64 targets on Windows.
 SET(CMAKE_GENERATOR_PLATFORM "ARM64")
 SET(CMAKE_SYSTEM_PROCESSOR "ARM64")
+SET(CMAKE_SYSTEM_NAME "Windows")
