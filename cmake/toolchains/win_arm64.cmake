7f5b9cefc70823c655e29e18c743589aa30739a9
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2023-12-05 19:33:33 +0100

    add toolchain file

---
+++ cmake/toolchains/win_arm64.cmake
@@ -0,0 +1,4 @@
+# Toolchain file that should provide required and non-conflicting 
+# build-parameters to allow normal and cross-compilation to ARM64 targets.
+SET(CMAKE_GENERATOR_PLATFORM ARM64)
+SET(CMAKE_SYSTEM_PROCESSOR ARM64)
